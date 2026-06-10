23b8561cad124af9ceb7989369c19e92ac856f2f
Santiago Pasaje <dspasaje89@gmail.com>
2026-06-10 14:09:10 -0500

    fix(frontend): validar que la variable sea un arreglo antes de usar .map en home 2

--- frontend/src/components/landing/pages/Home.jsx
+++ frontend/src/components/landing/pages/Home.jsx
@@ -248,7 +248,7 @@ const Home = () => {
         <section className="py-10 bg-bgPrimario">
           <div className="container mx-auto px-4 md:px-8">
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-              {Array.isArray(servicios).map((servicio, index) => (
+              {servicios.map((servicio, index) => (
                 <div key={index} className="text-center">
                   <div className="text-primario rounded-full border-2 border-gray-300 inline-flex items-center justify-center p-6 mb-2">
                     <servicio.icon className="size-8" />
@@ -281,7 +281,7 @@ const Home = () => {
 
             {/* categorias Grid */}
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-              {Array.isArray(categorias).map((categoria, index) => (
+              {categorias.map((categoria, index) => (
                 <CategoryCard key={index} {...categoria} />
               ))}
             </div>
@@ -424,9 +424,10 @@ const Home = () => {
             {!showSkeleton && !mostrarBanner && productosMostrar.length > 0 && (
               <div>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                  {Array.isArray(productosMostrar).map((producto, index) => (
-                    <ProductCard key={producto.id || index} {...producto} />
-                  ))}
+                  {Array.isArray(productosMostrar) &&
+                    productosMostrar.map((producto, index) => (
+                      <ProductCard key={producto.id || index} {...producto} />
+                    ))}
                 </div>
                 <div className="container mx-auto px-4 text-center pt-10">
                   <Link to="/auth">
